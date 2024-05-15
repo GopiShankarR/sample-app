3a835b072b49736c7d4acf5088f5d841c30614a8
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-05-15 11:39:56 -0500

    Todolist code push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import './App.css';
 import { useState } from "react";
+import { Task } from './Task';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: inputValue,
+      completed: false
+    }
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (taskId) => {
+    setTodoList(todoList.filter((task) => task.id !== taskId));
+  };
+
+  const changeCompleted = (taskId) => {
+    setTodoList(
+      todoList.map((task) => {
+        if(task.id === taskId) {
+          return {...task, completed: true}
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
   return (
     <div className='App'>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Set to Zero</button>
-      {count}
+      <div className='addTask'>
+        <input onChange={(event) => setInputValue(event.target.value)}></input>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='list'>
+        {todoList.map((task) => {
+          return <Task taskName={task.taskName} id={task.id} completed={task.completed} deleteTask={deleteTask} changeCompleted={changeCompleted}/>
+        })}
+      </div>
     </div>
   );
 }
