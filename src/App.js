fdd6d90203a8a9346123a6d1727d7f86f0c255ce
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-05-20 09:37:33 -0500

    experimented hooks with counters

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { Navbar } from './Navbar';
-import { createContext } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Form } from './components/Form';
-
-export const AppContext = createContext();
+import { useCount } from './useCount';
 
 function App() {
-  const client = new QueryClient({defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false
-    }
-  }});
+  const { count, increase, decrease, restart } = useCount();
+
   return (
     <div className='App'>
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-            <Routes>
-              <Route path='/' element={<Form />} />
-              <Route path='/profile' element={<Profile />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='*' element={<h2>THE PAGE YOU ARE SEARCHING IS NOT FOUND</h2>} />
-            </Routes>
-        </Router>
-      </QueryClientProvider>
+      {count}
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={restart}>Restart</button>
     </div>
   )
 }
